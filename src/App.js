398f1bd902105a6ff4a2bfe342dd552f354de920
Emin Kumru <eminkumru135@outlook.com>
2023-02-26 20:36:07 +0300

    added routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
